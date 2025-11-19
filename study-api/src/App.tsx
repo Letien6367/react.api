658a6554acdfb6bc7fba26fe069e30733b2ddc14
letien6367 <161240424+Letien6367@users.noreply.github.com>
2025-11-19 14:48:32 +0000

    update

--- study-api/src/App.tsx
+++ study-api/src/App.tsx
@@ -3,25 +3,41 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
-async function getRamdomCatURL(){
-  const apikey = ''
+import './style.scss'
+
+async function getRamdomCatURL() {
+  const apikey = 'live_abulHayZU0WrnHDun8A2oIW6HuG6qXPYkGuV6Q1HMe1SY87w5xGZQOwprFWxUSBh'
   const response = await axios.get("https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=" + apikey);
   return response.data[0].url;
 }
 
-function App() {
-  const [url,setUrl] = useState('');
 
+function App() {
+  const [url, setUrl] = useState('');
+  const [Isloading, setIsloading] = useState(false)
+  const handleClick = async () => {
+    setIsloading(true)
+    setUrl(await getRamdomCatURL())
+    setTimeout(() => {
+      setIsloading(false)
+    },1000 );
+  }
   return (
     <>
       <div>
-        <button onClick={async() => setUrl(await getRamdomCatURL())} >Click để lấy ảnh mèo</button>
+        <button onClick={async () => handleClick()} >Click để lấy ảnh mèo</button>
       </div>
       <div>
-        {
+        { url &&
           <img src={url} alt="Ảnh mèo" />
         }
       </div>
+      {Isloading && (
+          <div className="loading">
+            <h1>Đang Tải...</h1>
+          </div>
+         )
+        }
     </>
   )
 }
